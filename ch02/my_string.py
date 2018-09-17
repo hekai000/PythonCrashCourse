faad2ff8cbb61c67185862af2adafa125e2851b0
hekai000 <675974119@qq.com>
2018-09-17 10:54:43 +0800

    add my_string.py

---
+++ ch02/my_string.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+# !/usr/bin/env python
+
+'''
+
+@author: hekai
+
+@license: (C) Copyright 2013-2017, kylinos.
+
+@contact: hekai@kylinos.com.cn
+
+@software: garner
+
+@file: my_string.py.py
+
+@time: 2018/9/14 16:35
+
+@desc:
+
+'''
+name = "ada lovelace"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+quote = 'Albert Einstein once said,"A person who never made a mistake never tried anything new."'
+print(quote)
+
+famous_people = "Albert Einstein"
+quote = famous_people + 'once said,"A person who never made a mistake never tried anything new."'
+print(quote)
+print(0.2+0.1)
+print(3/2)
